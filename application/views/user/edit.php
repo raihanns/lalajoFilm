5f99eb6efd2415ec04284a6927a9ba2906f8b11b
raihanns <rainsdotnet@gmail.com>
2021-05-28 23:03:45 +0700

    Update edit.php

--- application/views/user/edit.php
+++ application/views/user/edit.php
@@ -11,7 +11,7 @@
             <div class="form-group row">
                 <label for="email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="email" name="email">
+                    <input type="text" class="form-control" id="email" name="email" value="<?= $user['email']; ?>" readonly>
                 </div>
             </div>
             <div class="form-group row">
@@ -21,12 +21,26 @@
                 </div>
             </div>
             <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">Full Name</label>
+                <div class="col-sm-2">Picture</div>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="name" name="name">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <img src="" class="img-thumbnail">
+                        </div>
+                        <div class="col-sm-9">
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="image" name="image">
+                                <label class="custom-file-label" for="image">Choose file</label>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
+            <div class="form-group row justify-content-end">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Edit</button>
+                </div>
+            </div>
 
 
         </div>
